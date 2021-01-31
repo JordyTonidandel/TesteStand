362493a9e8401f8f194b21999bf88567cf4bdf2e
JordyTonidandel <67643188+JordyTonidandel@users.noreply.github.com>
2021-01-31 15:49:00 -0300

    Inicializando arquitetura MVC

---
+++ Models/ClienteModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TesteStand.Models
+{
+    public class ClienteModel
+    {
+        public int Id { get; set; }
+
+        public string Razao_social { get; set; }
+
+        public string Cnpj { get; set; }
+
+        public DateTime Data_fundacao { get; set; }
+
+        public double Capital { get; set; }
+
+        public bool Quarentena { get; set; }
+
+        public bool Status_cliente { get; set; }
+
+        public char Classificacao { get; set; }
+    }
+}
